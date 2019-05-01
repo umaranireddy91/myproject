83c9df8c34f5cf9a4b727bcb8696380e62998a2b
Uma <umaranireddy91@gmail.com>
2019-05-01 11:19:39 -0400

    Added message to check the montreal branch

--- firstjava/src/Java_Class1/TestClass2.java
+++ firstjava/src/Java_Class1/TestClass2.java
@@ -4,6 +4,8 @@ public class TestClass2 {
 	// Created myMethod with return type void
         static void myMethod(String fname) {
 	    System.out.println(fname + " Empala");
+	    
+	    
 	  }
 	
 	// Created another Method with the name of checkMyAge
@@ -36,6 +38,7 @@ public class TestClass2 {
 	    checkMyAge(25); // checkMyAge calling
 	    
 	    System.out.println((myMethod1("uma", "Rani"))); 
+	    System.out.println("this is the message to check the montreal branch in my git server");
 	  }
 	  
 	  
